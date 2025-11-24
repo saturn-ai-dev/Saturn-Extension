83e666cbe2fe58bfe93e7b0a9916872626ff580d
Kian Pipalia <kian.pipalia@gmail.com>
2025-11-24 09:23:55 +0530

    Revert "Merge pull request #1 from saksham-loonker/mobileApp"
    This reverts commit 6ddae1f8e95f5a02c06b91a2adab6de10e95b311, reversing
    changes made to c65056d25d125400278e46e0d936925db9fc2479.

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.saturn.saturnAndroidTest1',
-  appName: 'saturn-android-raw-test',
-  webDir: 'dist'
-};
-
-export default config;
