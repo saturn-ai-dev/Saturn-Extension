fe5dfe0ec4207eeb48bd7117a0daf77380ae1282
Kian Pipalia <kian.pipalia@gmail.com>
2025-11-24 08:40:08 +0530

    basic capacitor import scripts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.saturn.saturnAndroidTest1',
+  appName: 'saturn-android-raw-test',
+  webDir: 'dist'
+};
+
+export default config;
